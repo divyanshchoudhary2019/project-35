7c5432277bbafe1018f0c562778f99d9911a7cfa
divyanshchoudhary2019 <58388053+divyanshchoudhary2019@users.noreply.github.com>
2020-05-18 12:16:33 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas, backgroundImage;
+
+var gameState = 0;
+var userCount=0;
+
+var database;
+
+var form, player, game;
+
+
+function setup(){
+  database=firebase.database();
+  canvas = createCanvas(displayWidth,displayHeight);
+ form1=new Form();
+ form1.display();
+ user=new User();
+
+}
+
+
+function draw(){
+
+
+
+
+}
+
